0d3f77abc4997d6bdb83d2f06557a933d263cae6
aitherium-sync <sync@aitherium.com>
2026-08-22 08:49:27 -0700

    sync: awseal 0.1.0 from monorepo

--- awseal/_doctor.py
+++ awseal/_doctor.py
@@ -22,7 +22,7 @@ import sys
 #: package cannot read the registry, and a doctor that guessed at the family
 #: would go stale in silence. Regenerate to update.
 SELF = 'awseal'
-FAMILY = ['awbac', 'awbrowse', 'awdit', 'awfind', 'awgit', 'awgraph', 'awiam', 'awkno', 'awm', 'awmail', 'awnboard', 'awnest', 'awnet', 'awprism', 'awreason', 'awrecover', 'awrecurse', 'awrelay', 'awrepl', 'awresearch', 'awrun', 'awshare', 'awtunnel']
+FAMILY = ['awbac', 'awbrowse', 'awdit', 'awfind', 'awgit', 'awgraph', 'awiam', 'awkno', 'awm', 'awmail', 'awnboard', 'awnest', 'awnet', 'awnode', 'awprism', 'awreason', 'awrecover', 'awrecurse', 'awrelay', 'awrepl', 'awresearch', 'awrun', 'awshare', 'awtunnel']
 PAIRS_WITH = ['awdit', 'awshare']
 
 
